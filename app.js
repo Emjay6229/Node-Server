9d22e09a2aa7470f36ca80e988ac335aee5bd1ca
Emjay6299 <emjay6299@gmail.com>
2023-03-10 01:21:09 +0100

    HTML-Server with Node HTTP module

---
+++ app.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+let port = 3000;
+
+let homePath = path.join(__dirname, "./Homepage.html");
+
+var server = http.createServer((req, res) => {
+    let url = req.url;
+    
+    if(url === "/"){
+        fs.readFile(homePath, (err, data) => {
+            if(err) throw err;
+            res.writeHead(200, {"content-type":"text/html"})
+            res.write(data);
+            res.end();
+        })
+    } else if (url === "/about"){
+        fs.readFile(aboutPath, (err, data) => {
+            if(err) throw err;
+            res.writeHead(200, {"content-type":"text/html"})
+            res.write(data);
+            res.end();
+        })
+    } else if (url === "/contact") {
+        fs.readFile(contactPath, (err, data) => {
+            if(err) throw err;
+            res.writeHead(200, {"content-type":"text/html"})
+            res.write(data);
+            res.end();
+        })
+    } else {
+        res.writeHead(404, {"content-type":"text/html"})
+        res.write("<h1>Page Not Found!</h1>")
+        res.end()
+    }
+})
+
+server.listen(port, () => {
+    console.log(`Server is listening at port ${port}...`)
+}) 
+
+
+module.exports = { server }
